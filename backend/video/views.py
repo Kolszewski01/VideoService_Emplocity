a35497ed84e19cfad61078e1b05033fe52878652
apelak <apelak10@wp.pl>
2024-03-26 17:57:11 +0100

    corrected search-bar

--- backend/video/views.py
+++ backend/video/views.py
@@ -78,14 +78,15 @@ def upload_video(request):
     video_url = request.build_absolute_uri(video.get_absolute_url())
     return HttpResponse(video_url)
 
+from django.shortcuts import render
+from .models import Video
+
 def search_feature(request):
-    # Check if the request is a post request.
     if request.method == 'POST':
-        # Retrieve the search query entered by the user
-        search_query = request.POST['search_query']
-        # Filter your model by the search query
-        posts = Model.objects.filter(title__contains=search_query)
-        posts += Model.objects.filter(tags__contains=search_query)
-        return render(request, 'templates/base.html', {'query':search_query, 'posts':posts})
+        search_query = request.POST.get('search_query', '')
+        # Użyj distinct() aby zwrócić unikalne wyniki wyszukiwania
+        posts = Video.objects.filter(title__icontains=search_query).distinct() | Video.objects.filter(tags__name__icontains=search_query).distinct()
+        return render(request, 'search_video.html', {'query': search_query, 'posts': posts})
     else:
-        return render(request, 'templates/base.html',{})
+        return render(request, 'search_video.html', {})
+
