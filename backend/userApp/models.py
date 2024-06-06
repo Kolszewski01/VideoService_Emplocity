d0dab0ef4b15056c8ffe768bc068f626cacefecc
lemkon24@gmail.com <42960067+Sebastian2103@users.noreply.github.com>
2024-06-06 06:22:59 +0200

    poprawki małe

--- backend/userApp/models.py
+++ backend/userApp/models.py
@@ -19,7 +19,7 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    avatar = models.ImageField(upload_to='users/avatars', blank=True, null=True, default=default_avatar)
+    avatar = models.ImageField(upload_to='users/avatars', blank=True, null=True, default='avatar.png')
 
     objects = CustomUserManager()
 
